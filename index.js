b4402c8a47f0cbb74ba31055fb33bb20380ca378
InsightsDevPabon <pabon@standard-insights.com>
2024-08-20 16:27:46 +0600

    Response Change

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.use(bodyParser.json());
 app.get("/webhook", (req, res) => {
 	console.log("Received Webhook (GET):", req.body);
 	const result = JSON.stringify(res.body);
-	res.json(result);
+	res.json({ message: "Success! Webhook GET", result });
 });
 
 app.post("/webhook", (req, res) => {
 	console.log("Received Webhook (POST):", req.body);
 	const result = JSON.stringify(res.body);
-	res.json(result);
+	res.json({ message: "Success! Webhook POST", result });
 });
 
 app.listen(PORT, () => {
